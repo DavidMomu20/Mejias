6a9b6804d2bbecd0ea65e6ef32417fb30eec83d9
DavidMomu20 <davidmoralesm2003@hotmail.com>
2023-05-30 20:39:19 +0200

    arreglo mis funciones

--- assets/js/misFunciones.js
+++ assets/js/misFunciones.js
@@ -4,14 +4,6 @@
 
 $(".select-2").select2();
 
-$(".tabla-crud").DataTable({
-  language: {
-    url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
-  },
-  responsive: true, 
-  scrollX: true
-});
-
 // ==============
 
 $(function() {
