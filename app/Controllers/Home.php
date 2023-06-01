0f1431ae32e9b5a38b87cbe7a5cf468ed3b4af5b
DavidMomu20 <davidmoralesm2003@hotmail.com>
2023-06-01 14:51:06 +0200

    comentario peticion curl a api

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -98,5 +98,37 @@ class Home extends BaseController
         } 
         else
             die('El archivo JSON no se encuentra.');
+
+        /* -- En esta parte muestro cómo hubiese sido el método si se hubiese llamado a la 
+            API directamente -- */
+
+        /*
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://local-business-data.p.rapidapi.com/business-reviews?business_id=0xd727be62b0bdea1%3A0x2dad48171be46d1f&limit=5&region=us&language=en",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "X-RapidAPI-Host: local-business-data.p.rapidapi.com",
+                "X-RapidAPI-Key: 63f5b6c8e4msh91e0a3a760a1bf3p194b1bjsnc6c157702e3f"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+        */
     }
 }
